ed559ddb1cb3ac283d4acb1301c6067519f89bdb
Jason Bourne <rustylake2000@qq.com>
2021-08-18 18:24:58 +0800

    Add some new files

---
+++ main.py
@@ -0,0 +1,9 @@
+from excel_operations import read_and_write as rw
+from function_operation import poly_fit as pf
+
+
+my_file = rw.read_file("test_file.xlsx")
+my_sheet = rw.read_sheet(my_file, 0)
+line1 = rw.read_row(my_sheet, 0, 0, 10)
+
+print(line1)
